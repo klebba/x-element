15cd387ca198aef494c4b031c9e7cd2387921f12
Casey Klebba <cklebba@netflix.com>
2017-10-25 14:15:23 -0700

    Fix path

--- x-element.js
+++ x-element.js
@@ -4,8 +4,8 @@
  */
 
 import XElementProperties from './x-element-properties.js';
-import { render, html } from '../../node_modules/lit-html/lib/lit-extended.js';
-import { repeat } from '../../node_modules/lit-html/lib/repeat.js';
+import { render, html } from '../../lit-html/lib/lit-extended.js';
+import { repeat } from '../../lit-html/lib/repeat.js';
 
 export default class AbstractElement extends XElementProperties {
   render() {
